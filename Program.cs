dffc19a1eee2892e2338a2dbdfe82c95132f06d4
Kirill <ivantee87@mail.ru>
2022-04-18 11:59:12 +0300

    Первое сохранение Маша

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите ваше имя :");
+string usermane = Console.ReadLine();
+
+if (usermane == "маша")
+{
+Console.Write("Ура, Это маша!!!");     
+}
+else
+{
+Console.Write("Привет,"); 
+Console.WriteLine(usermane);
+}
+
